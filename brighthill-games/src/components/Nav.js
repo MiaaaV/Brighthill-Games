f56e28b2a4abd90eeeca14a11f471072466c6e88
MiaaaV <sirivesa@gmail.com>
2024-05-01 17:58:14 +0300

    font added, nav done

--- brighthill-games/src/components/Nav.js
+++ brighthill-games/src/components/Nav.js
@@ -1,29 +1,53 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../images/logos/logo-white.png";
+import Twitter from "../images/icons/twitter.png";
+import Instagram from "../images/icons/instagram.png";
+import Facebook from "../images/icons/facebook.png";
+import YouTube from "../images/icons/youtube.png";
+import "./styles/Nav.css";
+import "../App.css";
 
 function Nav() {
 
   return (
     <>
-      <div>
-        <NavLink to="/">
-          logo
-          <img src={Logo} alt="Brighthill Games logo" />
-        </NavLink>
-
+      <div className="nav-container">
         <nav>
+          <NavLink to="/">
+            <img src={Logo} id="logo" alt="Brighthill Games logo" />
+          </NavLink>
+
           <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/games">games</NavLink></li>
-            <li><NavLink to="/careers">careers</NavLink></li>
-            <li><NavLink to="/about">about</NavLink></li>
-            <li><NavLink to="/news">news</NavLink></li>
+            <li className="nav-link"><NavLink to="/games">Games</NavLink></li>
+            <li className="nav-link"><NavLink to="/careers">Work with us</NavLink></li>
+            <li className="nav-link"><NavLink to="/about">About</NavLink></li>
+            <li className="nav-link"><NavLink to="/news">News</NavLink></li>
           </ul>
         </nav>
 
-        <div>
-          social icons
-        </div>
+        <ul className="icon-list">
+          <li>
+            <a href="#">
+              <img src={Twitter} id="facebook" className="social-icons" alt="Facebook icon" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Instagram} id="facebook" className="social-icons" alt="Facebook icon" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Facebook} id="facebook" className="social-icons" alt="Facebook icon" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={YouTube} id="facebook" className="social-icons" alt="Facebook icon" />
+            </a>
+          </li>
+        </ul>
+
       </div>
     </>
   )
