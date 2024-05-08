862a05bbafe96883df5e3193835475166219005b
MiaaaV <sirivesa@gmail.com>
2024-05-08 14:24:03 +0300

    link settings, redirections, home changes

--- brighthill-games/src/App.js
+++ brighthill-games/src/App.js
@@ -1,4 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { animateScroll } from 'react-scroll';
 import Home from "./routes/Home";
 import Games from "./routes/Games";
 import Careers from "./routes/Careers";
@@ -8,6 +11,33 @@ import Game from "./routes/Game";
 import './App.css';
 
 function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    // scrolls to desired #id if Link has one, otherwise scrolls to the top of the page
+    if (location.pathname && !location.pathname.includes('/:id')) {
+      if (location.hash) {
+        const targetId = location.hash.substring(1); // removes #
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          const navHeight = 144;
+          const sectionTop = targetElement.getBoundingClientRect().top;
+          const offset = sectionTop + window.scrollY - navHeight;
+          animateScroll.scrollTo(offset, {
+            smooth: true,
+            duration: 300
+          });
+        }
+      } else {
+        animateScroll.scrollToTop({
+          smooth: true,
+          duration: 300
+        });
+      }
+    }
+  }, [location]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
